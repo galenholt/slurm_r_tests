01c2b9db8f8b0c746967362b7639582a3b553f3c
Galen Holt <53928785+galenholt@users.noreply.github.com>
2023-03-09 13:19:46 +1100

    first pass at testing timeouts

--- master_timeouts.R
+++ master_timeouts.R
@@ -6,32 +6,13 @@ library(doFuture)
 library(future.batchtools)
 registerDoFuture()
 
-# Because `resources` gets parsed into the template, it's template-specific. So
-# set the template here
-
-# With my new template, I can now pass any slurm argument names. But if I want
-# to use those with '-', I need to replace it with '.' in its name in
-# `resources`
-
-
 # Declare the plan and see what it is. I should be able to use any of the
-# argument formats SLURM accepts. E.g. both time = "0:05:00" and time = 5 should
-# both work
-# This works.
-# plan(tweak(batchtools_slurm,
-#            template = "batchtools.slurm.tmpl",
-#            resources = list(time = "0:05:00",
-#                             ntasks.per.node = 2, 
-#                             mem = "1GB",
-#                             job.name = 'NewName')))
-
-# This is equivalent, just defining the options differently, and also works.
 plan(tweak(batchtools_slurm,
            template = "batchtools.slurm.tmpl",
-           resources = list(time = 5,
-                            ntasks.per.node = 2, 
-                            mem = 1000,
-                            job.name = 'NewName')))
+           resources = list(time = "0:02:00",
+                            ntasks.per.node = 1,
+                            mem = 200, # MB by default
+                            job.name = 'mastertest')))
 
 
 cat("\n Plan is:\n")
@@ -40,8 +21,14 @@ plan("list")
 
 
 
-# I'll use the same simple nested testing function as in `testing_plans.R`, at
-# least for the first pass
+# I'll modify the same simple nested testing function as in `testing_plans.R` to
+# produce output, which we can then see if it exists
+
+# Create directories to print into
+startdir <- file.path('testout', 'timeouts', 'startdir')
+enddir <- file.path('testout', 'timeouts', 'enddir')
+if (!dir.exists(startdir)) {dir.create(startdir, recursive = TRUE)}
+if (!dir.exists(enddir)) {dir.create(enddir, recursive = TRUE)}
 
 # The loop function
 nest_test <- function(outer_size, inner_size, planname) {
@@ -49,6 +36,11 @@ nest_test <- function(outer_size, inner_size, planname) {
                        .combine = bind_rows) %:% 
     foreach(j = 1:inner_size,
             .combine = bind_rows) %dopar% {
+              
+              # Create a file to see the loop started
+              file.create(file.path(startdir, 
+                                    paste0('outer_loop_', i, 
+                                           'inner_loop_', j, '.txt')))
               
               thisproc <- tibble(all_job_nodes = paste(Sys.getenv("SLURM_JOB_NODELIST"),
                                                        collapse = ","),
@@ -58,12 +50,21 @@ nest_test <- function(outer_size, inner_size, planname) {
                                  pid = Sys.getpid(),
                                  taskid = Sys.getenv("SLURM_LOCALID"),
                                  cpus_avail = Sys.getenv("SLURM_JOB_CPUS_PER_NODE"))
+              
+              # output the results of the loop
+              write.csv(thisproc, file = file.path(enddir, 
+                                                   paste0('outer_loop_', i,
+                                                          'inner_loop_', j,
+                                                          '.csv')))
+              
+              # still return the output of the whole thing so we can see if it finishes
+              thisproc
             }
   
   return(outer_out)
 }
 
-
+# We likely won't get to this bit- that's the point. But if we do, it's here.
 
 # What do we see for resources *before* we call anything?
 cat('\n### available workers:\n')
